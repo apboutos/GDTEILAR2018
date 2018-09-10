9ca0c9c76e5b25fbd9d9535c49b56c1b8b9dae32
exophrenik <exophrenik@gmail.com>
2018-09-10 16:05:19 +0300

    Order GET request implementation

--- app/src/main/java/com/exophrenik/grinia/order/OrderIntentService.java
+++ app/src/main/java/com/exophrenik/grinia/order/OrderIntentService.java
@@ -24,7 +24,7 @@ public class OrderIntentService extends IntentService{
     private HttpURLConnection connection;
     private boolean connectionError = false;
     private boolean orderResult = false;
-    private String orderInformation;
+    private String orderURL;
     private ArrayList<CartItem> cartList;
 
     public OrderIntentService() {
@@ -36,14 +36,21 @@ public class OrderIntentService extends IntentService{
         Log.d("RED","Hello world");
         try {
 
+
+            /*
             formatOrderInformationInJSON(intent);
 
-            createConnectionToServer();
+
 
             sendOrderInformationToServer();
 
-            readOrderResultFromServer();
 
+            */
+            formatOrderInformation(intent);
+
+            createConnectionToServer();
+
+            readOrderResultFromServer();
         }
         catch (Exception e) {
             Log.d("RED",e.getMessage(),e);
@@ -55,14 +62,14 @@ public class OrderIntentService extends IntentService{
         }
     }
 
+    /*
     private void formatOrderInformationInJSON(Intent intent) throws Exception{
 
         cartList = (ArrayList<CartItem>) intent.getSerializableExtra("cartList");
 
         JSONObject jsonObject = new JSONObject();
         JSONObject infoObject = new JSONObject();
-        //TODO fix username
-        infoObject.put("username","exophrenik");
+        infoObject.put("username",intent.getStringExtra("username"));
         infoObject.put("firstName",intent.getStringExtra("firstName"));
         infoObject.put("middleName",intent.getStringExtra("middleName"));
         infoObject.put("lastName",intent.getStringExtra("lastName"));
@@ -86,19 +93,85 @@ public class OrderIntentService extends IntentService{
         orderInformation = jsonObject.toString();
         Log.d("RED","JSON Object =" + orderInformation);
 
+    }*/
+
+    private void formatOrderInformation(Intent intent){
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("http://www.masterpaint.gr/order.php/?");
+
+        // User information
+
+        builder.append("username=");
+        builder.append(intent.getStringExtra("username"));
+        builder.append("&");
+        builder.append("firstName=");
+        builder.append(intent.getStringExtra("firstName"));
+        builder.append("&");
+        builder.append("middleName=");
+        builder.append(intent.getStringExtra("middleName"));
+        builder.append("&");
+        builder.append("lastName=");
+        builder.append(intent.getStringExtra("lastName"));
+        builder.append("&");
+        builder.append("address=");
+        builder.append(intent.getStringExtra("address"));
+        builder.append("&");
+        builder.append("postalCode=");
+        builder.append(intent.getStringExtra("postalCode"));
+        builder.append("&");
+        builder.append("creditCard=");
+        builder.append(intent.getStringExtra("creditCard"));
+        builder.append("&");
+        builder.append("expirationDate=");
+        builder.append(intent.getStringExtra("expirationDate"));
+        builder.append("&");
+        builder.append("cvs=");
+        builder.append(intent.getStringExtra("cvs"));
+
+        // Products List
+
+        cartList = (ArrayList<CartItem>) intent.getSerializableExtra("cartList");
+
+        builder.append("&");
+        builder.append("listSize=");
+        builder.append(cartList.size());
+
+
+        int index = 0;
+        for (CartItem item : cartList){
+
+            builder.append("&");
+            builder.append("barcode");
+            builder.append(index);
+            builder.append("=");
+            builder.append(item.getBarcode());
+            builder.append("&");
+            builder.append("quantity");
+            builder.append(index);
+            builder.append("=");
+            builder.append(item.getQuantity());
+            index++;
+        }
+
+        orderURL = builder.toString();
+
     }
+
 
     private void createConnectionToServer()throws Exception {
 
-        connection = (HttpURLConnection) new URL(getResources().getString(R.string.order_URL)).openConnection();
+        connection = (HttpURLConnection) new URL(orderURL).openConnection();
         connection.setDoOutput(true);
         connection.setDoInput(true);
         connection.setRequestProperty("Content-Type","application/json; charset=UTF-8");
         connection.setRequestProperty("Accept","application/json: charset=UTF-8");
-        connection.setRequestMethod("POST");
+        connection.setRequestMethod("GET");
         Log.d("RED","Request method " + connection.getRequestMethod());
     }
 
+    /*
     private void sendOrderInformationToServer() throws Exception{
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
         writer.write(orderInformation);
@@ -106,7 +179,7 @@ public class OrderIntentService extends IntentService{
         Log.d("RED","LOG: " + orderInformation); // Prints the sent JSON object.
         writer.close();
         Log.d("RED","Response Message " + connection.getResponseMessage());
-    }
+    }*/
 
     private void readOrderResultFromServer() throws Exception{
 
